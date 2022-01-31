f0cfac12ed97401c293a4fe61415e5223c391b15
Mirestrepot <92109447+Mirestrepot@users.noreply.github.com>
2022-01-31 11:01:21 -0500

    Definicion y costruccion codigo
    Inicializacion de variables, inicio de menú y camino de funciones para codigo
    
    Co-Authored-By: Valentinahs <93739630+Valentinahs@users.noreply.github.com>
    Co-Authored-By: valentinaguarin <73561521+valentinaguarin@users.noreply.github.com>
    Co-Authored-By: LuisaFGomez <93739212+LuisaFGomez@users.noreply.github.com>

--- CodigoHuffman.py
+++ CodigoHuffman.py
@@ -1,1 +1,30 @@
-print("Hola CodigoHuffman..") 
+class Nodes:  
+    
+    def __init__(self, probability, symbol, left = None, right = None):  
+        self.probability = probability  
+        self.symbol = symbol  
+        self.left = left  
+        self.right = right  
+        self.code = ''  
+              
+codes = dict() 
+
+while True:
+    try:
+
+        
+        entry = input("To exit enter: out \nEnter a text: ")
+        if entry == "out":
+            break
+
+        if all (c.isalpha() or c ==" "  for c in entry):
+            encoding, tree = FunctionEncoding(entry)  
+            
+            print("\tEncoded output: ", encoding, "\n")  
+            print("\tDecoded Output: ", FunctionDecoding(encoding, tree),"\n")
+
+
+    except :
+        print("Try again")
+        continue
+
